faf879a2dec6e4a9f2de0da0cbd006754705b411
Rajan kumar <rk2762992@gmail.com>
2025-11-14 14:01:27 +0530

    javaScript testing code

---
+++ javaScript/script.js
@@ -0,0 +1,22 @@
+console.log("Hello Rajan kumar");
+// let number = prompt("Please enter your");
+
+// if (number % 5 === 0) {
+//   console.log(number, "Yor number is multiple of 5");
+// } else {
+//   console.log(number, "Your number is  not number multiple of 5");
+// }
+
+let marks = prompt("Pleas enter your marks");
+
+if (marks >= 80) {
+  console.log("Your grade is A");
+} else if (marks >= 70) {
+  console.log("your Grade is B");
+} else if (marks >= 60) {
+  console.log("your grade is C");
+} else if (marks >= 50) {
+  console.log("your grade is D");
+} else {
+  console.log("Your grade is F");
+}
