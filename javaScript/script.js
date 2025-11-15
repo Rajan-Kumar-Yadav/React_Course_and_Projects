b707bd0ff6702cef6eb80dc82e5d9d6e590ebff3
Rajan kumar <rk2762992@gmail.com>
2025-11-15 10:37:04 +0530

    in javascript useing prompt and show @fullname123 fullName-lenth

--- javaScript/script.js
+++ javaScript/script.js
@@ -7,16 +7,20 @@ console.log("Hello Rajan kumar");
 //   console.log(number, "Your number is  not number multiple of 5");
 // }
 
-let marks = prompt("Pleas enter your marks");
+// let marks = prompt("Pleas enter your marks");
 
-if (marks >= 80) {
-  console.log("Your grade is A");
-} else if (marks >= 70) {
-  console.log("your Grade is B");
-} else if (marks >= 60) {
-  console.log("your grade is C");
-} else if (marks >= 50) {
-  console.log("your grade is D");
-} else {
-  console.log("Your grade is F");
-}
+// if (marks >= 80) {
+//   console.log("Your grade is A");
+// } else if (marks >= 70) {
+//   console.log("your Grade is B");
+// } else if (marks >= 60) {
+//   console.log("your grade is C");
+// } else if (marks >= 50) {
+//   console.log("your grade is D");
+// } else {
+//   console.log("Your grade is F");
+// }
+
+let fullName = prompt("Pleas enter your Full Name");
+
+console.log("@" + fullName + fullName.length);
