368596f79b4816ce87157ea1c80257010c5804ac
Rajan kumar <rk2762992@gmail.com>
2025-11-16 12:19:11 +0530

    Array Question Practise

--- javaScript/script.js
+++ javaScript/script.js
@@ -20,7 +20,51 @@ console.log("Hello Rajan kumar");
 // } else {
 //   console.log("Your grade is F");
 // }
+// //question
+// // let fullName = prompt("Pleas enter your Full Name");
 
-let fullName = prompt("Pleas enter your Full Name");
+// // console.log("@" + fullName + fullName.length);
 
-console.log("@" + fullName + fullName.length);
+// //Qs- print all elements of an array
+// let array = [8, 5, 2, 8, 5, 0, 34, 45, 23];
+// console.log(array);
+
+// //Qs- For a given arraywith marks of Students - [85,97,44,37,76,60] Find the average marks of the entire class.
+
+// let studentsMarks = [85, 97, 44, 37, 76, 60];
+// let averageMarks = 0;
+// let sum = 0;
+// for (let i = 0; i < studentsMarks.length; i++) {
+//   sum = sum + studentsMarks[i];
+//   averageMarks = sum / studentsMarks.length;
+// }
+// console.log(studentsMarks);
+// console.log("sum of the mrks is: ", sum);
+// console.log("Average of the marks is: ", averageMarks);
+
+// // Qs- For a given array with prices of 5 items -[250,645,300,900,50] All items have an offer of 10% OFF on them. Change the array to store  final price after applying offer.
+
+// let items = [250, 645, 300, 900, 50];
+// console.log(items);
+
+// for (let i = 0; i < items.length; i++) {
+//   let offer = (10 / 100) * items[i];
+//   items[i] = offer;
+// }
+// console.log("offer items");
+// console.log(items);
+
+// //Qs- Create an array to store companies->"Sloomberg","Microsoft","Uber","Google","IBM","Netfix"
+
+// let companies = ["Sloomberg", "Microsoft", "Uber", "Google", "IBM", "Netfix"];
+// console.log(companies);
+// //a.Remove the first company from the array,
+// // companies.shift("Sloomberg");
+// // console.log(companies);
+// //  b.Remove Uber and add Ola in its place,
+// companies.splice(2, 1, "Ola");
+// console.log(companies);
+
+// // c.Add Amazon at the end.
+// companies.push("Amazon");
+// console.log(companies);
