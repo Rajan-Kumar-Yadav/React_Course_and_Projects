ea90691fa47ca1853183b5b990fecc9b6f5ecca4
Rajan kumar <rk2762992@gmail.com>
2025-12-12 13:09:43 +0530

    spreade operator use and Functional-update use

---
+++ 34SpreadOperator-And-Functional-update/5-toto-app-version-four/src/App.jsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import "./App.css";
+import AddTodo from "./Components/AddTodo";
+import AppName from "./Components/ApppName";
+import TodoItem from "./Components/TodoItem";
+import WelcomeMessage from "./Components/WelcomeMessage";
+
+function App() {
+  const [todoItems, setTodoItems] = useState([]);
+  const handleNewItem = (itemName, itemDueDate) => {
+    console.log(`New Item Added: ${itemName} Date:${itemDueDate}`);
+
+    setTodoItems((currValue) => {
+      const newTodoItems = [
+        ...currValue,
+        { todoName: itemName, date: itemDueDate },
+      ];
+      return newTodoItems;
+    });
+  };
+
+  const handleDeleteItem = (todoItemName) => {
+    console.log(`Item Deleted: ${todoItemName}`);
+    const newTodoItems = todoItems.filter(
+      (item) => item.todoName !== todoItemName
+    );
+    setTodoItems(newTodoItems);
+  };
+
+  return (
+    <>
+      <center className="todo-container">
+        <AppName />
+        <AddTodo onNewItem={handleNewItem} />
+        {todoItems.length === 0 && <WelcomeMessage />}
+
+        <TodoItem todoItems={todoItems} onDeleteClick={handleDeleteItem} />
+      </center>
+    </>
+  );
+}
+
+export default App;
