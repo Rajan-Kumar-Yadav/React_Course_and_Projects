df46caa72c4a2b9962f87a3f651143dde7d2d36a
Rajan kumar <rk2762992@gmail.com>
2025-12-16 11:31:18 +0530

    37 dummy Api 38 Data Fetching Using Fetch

---
+++ 34SpreadOperator-And-Functional-update/37-Dummy-Api-38-Fetching-Data/7-social-media/src/components/WelcomeMessage.jsx
@@ -0,0 +1,15 @@
+const WelcomeMessage = ({ onGetPostsClick }) => {
+  return (
+    <center className="welcome-message">
+      <h1>There are no posts</h1>;
+      <button
+        type="button"
+        className="btn btn-primary"
+        onClick={onGetPostsClick}
+      >
+        Fetch Data
+      </button>
+    </center>
+  );
+};
+export default WelcomeMessage;
