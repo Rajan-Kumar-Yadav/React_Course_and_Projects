ddd7c85fc99f3407ee98524a64479c21c821b886
Rajan kumar <rk2762992@gmail.com>
2025-12-17 12:46:18 +0530

    useEfeect using to page reloading

---
+++ 34SpreadOperator-And-Functional-update/38-UseEfeect-Hook/7-social-media/src/components/PostList.jsx
@@ -0,0 +1,27 @@
+import { useContext, useEffect, useState } from "react";
+import Post from "./Post";
+import { PostListCreateContext } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
+import LoadingSpinner from "./LoadingSpinner";
+
+const PostList = () => {
+  const { postList, addInitialPosts } = useContext(PostListCreateContext);
+  const [fetching, setFetching] = useState(true);
+  useEffect(() => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then((data) => {
+        addInitialPosts(data.posts);
+        setFetching(false);
+      });
+  }, []);
+
+  return (
+    <>
+      {fetching && <LoadingSpinner />}
+      {!fetching && postList.length === 0 && <WelcomeMessage />}
+      {!fetching && postList.map((post) => <Post key={post.id} post={post} />)}
+    </>
+  );
+};
+export default PostList;
